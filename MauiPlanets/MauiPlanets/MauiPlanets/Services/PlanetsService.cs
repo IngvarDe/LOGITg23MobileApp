a28a54ecbfac48c3af55ce61c42f2648ffbec37c
Ingvar Derešivski <inde999@gmail.com>
2024-12-10 09:57:33 +0200

    InnerPlanets

--- MauiPlanets/MauiPlanets/MauiPlanets/Services/PlanetsService.cs
+++ MauiPlanets/MauiPlanets/MauiPlanets/Services/PlanetsService.cs
@@ -24,7 +24,65 @@ namespace Services
                     "https://solarsystem.nasa.gov/system/feature_items/images/73_carousel_mercury_2.jpg",
                     "https://solarsystem.nasa.gov/system/feature_items/images/75_mercury_carousel_1.jpg"
                 }
-            }
+            },
+            new()
+            {
+                Name = "Venus",
+                Subtitle = "The pressure cooker",
+                HeroImage = "venus.png",
+                Description = "Venus is the second planet from the Sun. It is a " +
+                "terrestrial planet and is the closest in mass and size to its " +
+                "orbital neighbour Earth. Venus has by far the densest atmosphere " +
+                "of the terrestrial planets, composed mostly of carbon dioxide " +
+                "with a thick, global sulfuric acid cloud cover.",
+                AccentColorStart = Color.FromArgb("#a6393b"),
+                AccentColorEnd = Color.FromArgb("#d17f21"),
+                Images = new()
+                {
+                    "https://solarsystem.nasa.gov/system/feature_items/images/143_Venus-800x600.jpg",
+                    "https://solarsystem.nasa.gov/system/feature_items/images/147_Hubble_Venus_Clouds-800x600.jpg",
+                    "https://solarsystem.nasa.gov/system/feature_items/images/146_venus_carousel_3.jpg",
+                    "https://solarsystem.nasa.gov/system/feature_items/images/144_PIA00485_Venus_Caldera-800.jpg"
+                }
+            },
+            new()
+            {
+                Name = "Earth",
+                Subtitle = "The cradle of life",
+                HeroImage = "earth.png",
+                Description = "Earth is the third planet from the Sun and " +
+                "the only astronomical object known to harbor life. This is " +
+                "enabled by Earth being an ocean world, the only one in the " +
+                "Solar System sustaining liquid surface water. ",
+                AccentColorStart = Color.FromArgb("#0e3d68"),
+                AccentColorEnd = Color.FromArgb("#2e97c7"),
+                Images = new()
+                {
+                    "https://solarsystem.nasa.gov/system/feature_items/images/68_epicearthmoonstill_800.png",
+                    "https://solarsystem.nasa.gov/system/feature_items/images/69_earth_carousel_3.jpg",
+                    "https://images.newscientist.com/wp-content/uploads/2019/09/09162708/iss048-e-2035_lrg.jpg?width=778"
+                }
+            },
+            new()
+            {
+                Name = "Mars",
+                Subtitle = "The red beauty",
+                HeroImage = "mars.png",
+                Description = "No planet has sparked the imaginations of humans as much " +
+                "as Mars. It may be the reddish color of Mars, or the fact that it can " +
+                "often be easily seen in the night sky, that has caused people to wonder " +
+                "about this close neighbor of ours. Tales of “Martians” invading Earth " +
+                "have been around for well over fifty years. But is it likely that any " +
+                "kind of life really does exist on Mars? ",
+                AccentColorStart = Color.FromArgb("#a23036"),
+                AccentColorEnd = Color.FromArgb("#eb3333"),
+                Images = new()
+                {
+                    "https://researchfdi.com/wp-content/uploads/2021/10/8944_1-PIA24546-1280.jpeg",
+                    "https://www.worldatlas.com/r/w1300-q80/upload/bb/c3/32/shutterstock-1041249343.jpg",
+                    "https://www.openaccessgovernment.org/wp-content/uploads/2021/04/dreamstime_xxl_121672573-scaled.jpg"
+                }
+            },
         };
 
         public static List<Planet> GetFeaturedPlanets()

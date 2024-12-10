30a11a1371be7a626cf9df465115ef615328ac5f
Ingvar Derešivski <inde999@gmail.com>
2024-12-10 09:15:45 +0200

    PlanetsdetailsPage

--- MauiPlanets/MauiPlanets/MauiPlanets/Views/PlanetsDetailsPage.xaml.cs
+++ MauiPlanets/MauiPlanets/MauiPlanets/Views/PlanetsDetailsPage.xaml.cs
@@ -7,5 +7,12 @@ public partial class PlanetsDetailsPage : ContentPage
 	public PlanetsDetailsPage(Planet planet)
 	{
 		InitializeComponent();
+
+		this.BindingContext = planet;
+	}
+
+	async void BackButton_Clicked(System.Object sender, System.EventArgs e)
+	{
+		await Navigation.PopAsync();
 	}
 }
